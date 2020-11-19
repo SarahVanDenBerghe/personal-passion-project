126fe3b1c3c6ca7e7e11ec399d37658b3859ca50
Sarah Van Den Berghe <sarah.van.den.berghe@student.howest.be>
2020-11-19 15:50:47 +0100

    created basic strapi server

---
+++ server/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '0437e3bd035d1512dacc7cac40c9803d'),
+    },
+  },
+});
