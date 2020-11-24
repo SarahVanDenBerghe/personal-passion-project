0a25ced57c3bb5696a07f30188641ea7f4e09795
Sarah Van Den Berghe <sarah.van.den.berghe@student.howest.be>
2020-11-24 20:25:25 +0100

    changed api link in env files

--- src/contexts/BaublesContext.js
+++ src/contexts/BaublesContext.js
@@ -1,13 +1,12 @@
 import React, { createContext, useState, useEffect } from 'react';
 import axios from 'axios';
-// https://www.youtube.com/watch?v=35lXWvCuM8o
 
 // CONTEXT **************** */
 export const BaublesContext = createContext();
 
 // PROVIDER **************** */
 const api = axios.create({
-  baseURL: `${process.env.REACT_APP_STRAPI_API}?_limit=-1`,
+  baseURL: `${process.env.REACT_APP_STRAPI_API}/messages?_limit=-1`,
 });
 
 const BaublesProvider = ({children}) => {
