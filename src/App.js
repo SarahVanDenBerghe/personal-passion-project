bdcc9567f6b380a4564551ee3eb8fc4ef9df0688
Sarah Van Den Berghe <sarah.van.den.berghe@student.howest.be>
2020-12-08 03:55:18 +0100

    small changes for demo

--- src/App.js
+++ src/App.js
@@ -1,45 +1,38 @@
-import React, { useRef, useState, useEffect } from "react";
-import { Canvas } from "react-three-fiber";
-import { softShadows, OrbitControls } from "drei";
-import { Header } from "./components/ui";
-import "./App.scss";
+import React, { useRef, useState, useEffect } from 'react';
+import { Canvas } from 'react-three-fiber';
+import { softShadows, OrbitControls } from 'drei';
+import { Header } from './components/ui';
+import './App.scss';
 import { Lights, Tree, Floor, Ball } from './components/scene';
 
 softShadows();
 
 const App = () => {
   const [baubles, setBaubles] = useState([]);
-  const [baublePreview, setBaublePreview] = useState(null); 
+  const [baublePreview, setBaublePreview] = useState(null);
 
   return (
     <>
       <Header />
-      <Canvas
-        colorManagement
-        shadowMap
-        camera={{ position: [-7, 10, 3], fov: 60 }}
-        background={'pink'}>
+      <Canvas colorManagement shadowMap camera={{ position: [-7, 10, 3], fov: 60 }}>
         <Lights />
-          <group>
+        <group>
           <Floor />
           <Tree
             baubles={baubles}
             setBaubles={setBaubles}
             setBaublePreview={setBaublePreview}
             position={[0, 0, 0]}
-            color='#669966'
+            color="#669966"
             args={[3, 9, 9]}
             speed={2}
           />
-          {baubles.map(bauble => { 
-            return bauble
+          {baubles.map((bauble) => {
+            return bauble;
           })}
           {baublePreview ? baublePreview : ''};
-
         </group>
-        <OrbitControls 
-          // enableZoom={false}
-        />
+        <OrbitControls />
       </Canvas>
     </>
   );
