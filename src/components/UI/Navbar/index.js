d8735818f45fe286225364db31846af16c3a05c4
Sarah Van Den Berghe <sarah.van.den.berghe@student.howest.be>
2020-12-15 13:34:37 +0100

    headr icon to go back to home

--- src/components/UI/Navbar/index.js
+++ src/components/UI/Navbar/index.js
@@ -1,10 +1,18 @@
 import React, { useEffect, useRef, useState } from 'react';
 import { gsap } from 'gsap';
+import { Link } from 'react-router-dom';
+import { ROUTES } from '../../../consts';
 import styles from './styles.module.scss';
 
 const Navbar = () => {
   const [audio, setAudio] = useState(null);
   const [playMusic, setPlayMusic] = useState(false);
+
+  let line,
+    music,
+    musicLine,
+    tree,
+    treeLine = useRef(null);
 
   const toggleMusic = () => {
     if (playMusic) {
@@ -30,49 +38,47 @@ const Navbar = () => {
     if (!audio) {
       const audioFromFile = new Audio('/assets/music/background.mp3');
       audioFromFile.loop = true;
-      audioFromFile.volume = 0.04;
+      audioFromFile.volume = 0.1;
       setAudio(audioFromFile);
     }
   }, []);
-
-  let line,
-    musicLine = useRef(null);
-
-  let hamburgerOne,
-    hamburgerTwo,
-    hamburgerThree,
-    music = useRef(null);
 
   useEffect(() => {
     gsap.to(line, {
       duration: 0.85,
       ease: 'Power2.easeIn',
       height: '100%',
-      delay: 0.15,
+      delay: 0.35,
     });
 
-    gsap.to([hamburgerOne, hamburgerTwo, hamburgerThree], {
-      duration: 0.55,
-      width: '100%',
-      delay: 0.15,
-      stagger: {
-        amount: 0.2,
-      },
+    gsap.to(tree, {
+      duration: 0.5,
+      ease: 'back.out(2.2)',
+      scaleX: 1,
+      scaleY: 1,
+      delay: 1.2,
+    });
+
+    gsap.to(treeLine, {
+      duration: 0.7,
+      ease: 'Power2.easeOut',
+      scaleY: 1,
+      delay: 1.3,
     });
 
     gsap.to(music, {
       duration: 0.3,
-      ease: 'Power2.easeIn',
+      ease: 'back.out(2.2)',
       scaleX: 1,
       scaleY: 1,
-      delay: 0.5,
+      delay: 1.7,
     });
 
     gsap.to(musicLine, {
       duration: 0.5,
       ease: 'Power2.easeIn',
       height: '3rem',
-      delay: 0.8,
+      delay: 1.7,
     });
   }, []);
 
@@ -80,23 +86,26 @@ const Navbar = () => {
     <header className={styles.header}>
       <div className={styles.header__content}>
         <nav className={styles.nav}>
-          <div className={styles.nav__icon}>
-            <span
-              ref={(el) => {
-                hamburgerOne = el;
-              }}
-            />
-            <span
-              ref={(el) => {
-                hamburgerTwo = el;
-              }}
-            />
-            <span
-              ref={(el) => {
-                hamburgerThree = el;
-              }}
-            />
-          </div>
+          <Link className={styles.button} to={ROUTES.home}>
+            <div className={styles.tree}>
+              <img
+                height="40"
+                width="40"
+                ref={(el) => {
+                  tree = el;
+                }}
+                className={styles.header__tree}
+                alt="volume icon"
+                src="/assets/icons/tree-circle.svg"
+              />
+              <span
+                ref={(el) => {
+                  treeLine = el;
+                }}
+                className={styles.tree__line}
+              />
+            </div>
+          </Link>
         </nav>
         <button aria-label="Toggle music" className={styles.volume} onClick={() => toggleMusic()}>
           <img
